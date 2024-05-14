32e794ac9c6845a34ea99a4a291c1eec374e0c78
Sergey Marchenko <yegres.marchenko@gmail.com>
2024-05-14 12:21:52 -0700

    cswdef

--- test2file.py
+++ test2file.py
@@ -4,7 +4,7 @@
 # import numpy as np
 
 st.write("Welcome to the new app!")
-x = np.arange(0, 11, 1)
+# x = np.arange(0, 11, 1)
 
 # rng = np.random.default_rng()
 # y = rng.integers(0, high=10, size=(1,10))
