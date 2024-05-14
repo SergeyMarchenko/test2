504f76dbff59e3e7c39e405cd25a1949030e2945
Sergey Marchenko <yegres.marchenko@gmail.com>
2024-05-14 13:39:01 -0700

    rgfre

--- test2file.py
+++ test2file.py
@@ -1,13 +1,23 @@
 import streamlit as st
 import numpy     as np
+# from   sqlalchemy import create_engine
+# from   sqlalchemy import inspect
+
 
 # import plotly.graph_objects as go    
 # import pandas               as pd
 
 
 st.write("Welcome to the new app!")
-x = np.arange(0, 11, 1)
+x = np.arange(0, 11, 11)
 st.write(x)
+
+url = "mysql+mysqlconnector://viuhydro_shiny:.rt_BKD_SB*Q@192.99.62.147:3306/viuhydro_wx_data_v2"
+st.write(url)
+# engine = create_engine(url)
+# inspection   = inspect(engine)
+# table_names  = inspection.get_table_names("viuhydro_wx_data_v2")
+# table_names = [x for x in table_names if x.startswith("raw")]
 
 # rng = np.random.default_rng()
 # y = rng.integers(0, high=10, size=(1,10))
