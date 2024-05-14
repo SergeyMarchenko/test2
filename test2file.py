118cfbb9a7929e5f44ddb6884e88f329e0354d59
Sergey Marchenko <yegres.marchenko@gmail.com>
2024-05-14 14:44:05 -0700

    vervfer

--- test2file.py
+++ test2file.py
@@ -13,11 +13,11 @@ x = np.arange(0, 11, 1)
 st.write(x)
 
 
-st.write(url)
+st.write(st.secrets.config_username)
 
 
 st.write("added code using functions from sqlalchemy")
-engine = create_engine(url)
+# engine = create_engine(url)
 # inspection   = inspect(engine)
 # table_names  = inspection.get_table_names("viuhydro_wx_data_v2")
 # table_names = [x for x in table_names if x.startswith("raw")]
